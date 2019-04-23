ff0ec82167679e2b52516d2215a2b0b696fb51cd
Mahashraya Sundararaman <mahashraya@Mahashrayas-MacBook-Pro.local>
2019-04-23 13:09:07 -0400

    almost there2

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,86 +1,87 @@
-function buildMetadata(sample) {
-
-  // @TODO: Complete the following function that builds the metadata panel
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-        // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-
-  var url = `/metadata/${sample}`
-  d3.json(url).then(function(sample) {
-    var selectPanel = d3.select("#sample-metadata");
-    selectPanel.html("");
-    Object.entries(sample).forEach(function ([key, value]) {
-      var row = selectPanel.append("p");
-      row.text(`${key}: ${value}`);
-    });
-      }
-  )};
-
-function buildCharts(sample) {
-
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-
-  var url = `/samples/${sample}` 
-  d3.json(url).then(function(data) {
-    var x_values = data.otu_ids;
-    var y_values = data.sample_values;
-    var m_size = data.sample_values;
-    var m_colors = data.otu_ids;
-    var t_values = data.otu_labels;
-
-    var trace1 = {
-      x: x_values,
-      y: y_values,
-      text: t_values,
-      mode: "markers",
-      marker: {
-        color: m_colors,
-        size: m_size
-      }
-    };
-
-    Plotly.newPlot('bubble', data, layout);
-
-
-
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = Plotly.d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+      sampleNames.forEach((sample) => {
+          selector
+              .append("option")
+              .text(sample)
+              .property("value", sample);
+      });
+      dropdown_select.data(response)
+      .enter()
+      .append("option")
+      .attr("value",data)
+      .text(data)
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
+      const firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
   });
 }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+function buildMetadata(sample) {
+  var url = `/metadata/${sample}`
+  d3.json(url).then(function(sample) {
+      var selectMetadata = d3.select("#sample-metadata");
+      selectMetadata.html("");
+      Object.defineProperties(sample).forEach(function ([key, value]) {
+          var row = selectMetadata.append("p");
+          row.text(`${key}: ${value}`);
+      });
+  });
+};
+
+function buildBubble(sample) {
+  var url = `/samples/${sample}`
+  d3.json(url).then(function(data) {
+      var x_values = data.otu_ids;
+      var y_values = data.sample_values;
+      var m_size = data.sample_values;
+      var m_colors = data.otu_ids;
+      var t_values = data.otu_labels;
+
+      var layout = {
+          x: x_values,
+          y: y_values,
+          textz: t_values,
+          mode: "markers",
+          marker: {
+              color: m_colors,
+              size: m_size
+          }
+      };
+
+      Plotly.newPlot("bubble", data, layout)
+  });
+};
+
+function buildPie(sample) {
+  var url = `/samples/${sample}`
+  d3.json(url).then(function(data) {
+      var pie_labels = data.otu_ids.slice(0,11);
+      var pie_values = data.sample_values.slice(0,11);
+      var pie_desc = data.otu_labels.slice(0,11);
+
+      var layout = {
+          values: pie_values,
+          labels: pie_labels,
+          type: "pie",
+          name: "Top 10 Bellybutton Biodiversity Samples",
+          textinfo: "percent",
+          text: pie_desc,
+          textposition: "inside",
+          hoverinfo: "label+value+text+percent" 
+      }
+      Plotly.newplot("pie", data, layout)
+  })
 }
 
-// Initialize the dashboard
+function optionChanged(newSample) {
+  console.log("optionchanged detected and new sample selected")
+  console.log("new sample: " + newSample )
+  buildMetadata(newSample);
+  buildBubble(newSample);
+  buildPie(newSample);
+}
 init();
